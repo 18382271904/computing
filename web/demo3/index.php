c094dfaef100515408758bf173651c00c60ed4c3
18382271904 <31686743+18382271904@users.noreply.github.com>
2017-09-10 16:04:53 +0800

    四则运算
    随机生成

---
+++ web/demo3/index.php
@@ -0,0 +1,33 @@
+<?php
+//require('../demo2/index.php');
+/*$ma1=$_POST['a1'];
+$ma2=$_POST['a2'];
+$ma3=$_POST['a3'];
+$ma4=$_POST['a4'];
+$ma5=$_POST['a5'];
+$ma6=$_POST['a6'];
+$ma7=$_POST['a7'];
+$ma8=$_POST['a8'];
+*/
+?>
+<!doctype html>
+<html lang="zh">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>成绩查看</title>
+
+<link rel="stylesheet" href="css/clouds.css" />
+
+</head>
+<body>
+<div id="far-clouds" class="stage far-clouds"></div>
+<div id="near-clouds" class="stage near-clouds"></div>
+
+<script src="js/jquery-1.11.0.min.js" type="text/javascript"></script>
+<script src="js/clouds.js"></script>
+<script src="js/app.js"></script>
+</div>
+</body>
+</html>
